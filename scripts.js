312f53793d4728ff563cf2da60e4d8ae23919be8
talosss <tao-chin@hotmail.com>
2022-05-29 19:53:27 -0300

    assds

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function mostrando(){
+    document.getElementById("card-bodyss2").style.display = "block" 
+
+}
+
+function ocultando(){
+    document.getElementById("card-bodyss2").style.display = "none" 
+
+}
