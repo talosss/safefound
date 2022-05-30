f5f9cbb4d7bc2e5cb112ed53a0e174f5a1f71b18
talosss <tao-chin@hotmail.com>
2022-05-29 22:35:14 -0300

    finish

--- scripts.js
+++ scripts.js
@@ -1,9 +1,31 @@
+const { get } = require("http")
+
 function mostrando(){
     document.getElementById("card-bodyss2").style.display = "block" 
-
+    document.getElementById("cajaswap").style.display = "none"
 }
 
 function ocultando(){
     document.getElementById("card-bodyss2").style.display = "none" 
-
+    document.getElementById("cajaswap").style.display = "block"
 }
+ 
+function addStokens(){
+    document.getElementById("addtoken").style.display = "block" 
+    document.getElementById("cajaswap").style.display = "none"
+}
+  function OcultandoADStokens(){
+      document.getElementById("addtoken").style.display = "none"
+      document.getElementById("cajaswap").style.display = "block"
+      
+
+  }
+  function Conectwallet(){
+    document.getElementById("conectwallet").style.display = "block"
+    document.getElementById("cajaswap").style.display =    "none"
+  }
+
+  function OCultandoconectwallet(){
+    document.getElementById("conectwallet").style.display = "none"
+    document.getElementById("cajaswap").style.display =    "block"
+  }
